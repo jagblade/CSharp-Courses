4695f7ec816c39d88e04c278fb07fb09c98d032d
jagblade <jagblade@abv.bg>
2022-04-09 22:30:31 +0300

    One Task - ExtractPersonInformation / TextProcessing

---
+++ Kurs-C#-Fundamentals/E07.AssociativeArrays/PE01.Ranking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PE01.Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
