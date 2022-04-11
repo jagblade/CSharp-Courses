1652410cb2a9fe5eeaeb581d352d3cbc5932d9e6
jagblade <jagblade@abv.bg>
2022-04-11 22:25:58 +0300

    Methods/ 3 and 4 th tasks

---
+++ Kurs-C#-Fundamentals/04. Methods/E04. Methods/PE04.TribonacciSequence/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PE04.TribonacciSequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintNthTribonacciNumber(n);
+        }
+
+        private static void PrintNthTribonacciNumber(int n)
+        {
+            long[] sequnceLast3Nums = new long[3];
+            sequnceLast3Nums[0] = 1;
+            sequnceLast3Nums[1] = 1;
+            sequnceLast3Nums[2] = 2;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i > 2)
+                {
+                    long temp = sequnceLast3Nums[0];
+                    sequnceLast3Nums[0] = sequnceLast3Nums[1];
+                    sequnceLast3Nums[1] = sequnceLast3Nums[2];
+                    sequnceLast3Nums[2] = sequnceLast3Nums[0] + sequnceLast3Nums[1] + temp;
+
+                    Console.Write($"{sequnceLast3Nums[2]} ");
+                }
+                else
+                {
+                    Console.Write($"{sequnceLast3Nums[i]} ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
