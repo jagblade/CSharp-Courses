c7b4ff455a038131b9bbd17c94b7708cbbada87e
jagblade <jagblade@abv.bg>
2022-05-26 23:29:52 +0300

    Sets-and-Dictionaries-Advanced-Exercise/ 3 tasks

---
+++ Kurs-C#-Advanced-may-2022/Sets-and-Dictionaries-Advanced-Exercise/P06.Wardrobe/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            //запис: цвят -> {дреха -> бр. срещания}
+            //1. цвят -> ако го няма (добавяме), ако го има:
+            //2. дреха -> ако няма (добавяме към съответния цвят), ако има: + 1
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                //"Blue -> dress,jeans,hat".Split(" -> ") -> ["Blue", "dress,jeans,hat"]
+                string color = input.Split(" -> ")[0];
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    //нямаме такъв цвят
+                    //цвят -> празен речник за дрехите
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+                //имаме такъв цвят
+                //взимаме дрехите от моя цвят
+                Dictionary<string, int> clothes = wardrobe[color]; //дрехите от съответния цвят
+                string[] inputClothes = input.Split(" -> ") //["Blue", "dress,jeans,hat"]
+                                        [1]                 //"dress, jeans, hat"
+                                        .Split(",");        //["dress", "jeans", "hat"]
+
+                foreach (string cloth in inputClothes)
+                {
+                    if (!clothes.ContainsKey(cloth))
+                    {
+                        clothes.Add(cloth, 1);
+                    }
+                    else
+                    {
+                        clothes[cloth]++;
+                    }
+                }
+            }
+
+            string searchedItems = Console.ReadLine(); // "Blue dress"
+            string searchedColor = searchedItems.Split(" ")[0];
+            string searchedCloth = searchedItems.Split(" ")[1];
+
+            foreach (var colorEntry in wardrobe)
+            {
+                //запис за цвета:
+                //key (цвят)
+                //value (речник с дрехите)
+
+                Console.WriteLine($"{colorEntry.Key} clothes:");
+                Dictionary<string, int> clothes = colorEntry.Value;
+                //запис за дрехите: дреха -> бр. срещанията
+                foreach (var cloth in clothes)
+                {
+                    //запис за дреха:
+                    //key (име на дрехата)
+                    //value (бр. срещания)
+                    if (cloth.Key == searchedCloth && colorEntry.Key == searchedColor)
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Kurs-C#-Advanced-may-2022/Sets-and-Dictionaries-Advanced-Exercise/Sortings/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CSharpAdvancedMay2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //7.
+            //влогър -> списък с последователи (followers)
+            //влогър -> списък със следващи (following)
+
+            Dictionary<string, List<string>> followers = new Dictionary<string, List<string>>();
+            //спрямо брой на последователи
+            followers.OrderBy(entry => entry.Value.Count);
+
+
+            Dictionary<string, List<string>> following = new Dictionary<string, List<string>>();
+            //сортираме във низходящ ред спрямо брой последованите -> сортираме по име (a - z)
+            //Иван ->  {Петър, Георги, Тишо} 3
+            //Георги ->  {Петър, Иван, Тишо, Мартин} 4
+            //Мартин ->  {Петър, Иван, Тишо} 3
+            following
+                .OrderByDescending(entry => entry.Value.Count) //1. Георги   2. Иван    3.Мартин
+                .ThenBy(entry => entry.Key);                   //1. Георги   2. Иван   3. Мартин
+
+            //descending (намаляващ ред): z -> a; 10 -> 0
+            //ascending (нарастващ ред): a -> z; 0 -> 10
+
+            //8. 
+            //студент: (курс -> точки)
+            Dictionary<string, Dictionary<string, int>> students = new Dictionary<string, Dictionary<string, int>>();
+
+            //1. име на студентите (нарастващ ред)
+            //2. намаляващ ред на точките
+
+            students
+                .OrderBy(entry => entry.Key); //entry => key: име на студент, value: речник с курсове
+
+            foreach (var studentEntry in students)
+            {
+                //studentEntry:  key: име на студент, value: речник с курсове
+                Dictionary<string, int> courses = studentEntry.Value;
+                //key: курс, value: точки от курса
+                courses.OrderByDescending(entry => entry.Value);
+            }
+
+        }
+    }
+}
