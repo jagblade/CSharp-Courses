670d370da83140fcc83648c21af1bfc239ecd00b
jagblade <jagblade@abv.bg>
2022-06-11 23:21:27 +0300

    Functional Programming - Exercise\ 2 tasks

---
+++ Kurs-C#-Advanced-may-2022/FunctionalProgramming-Exercise/P01.ActionPoint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split().ToList();
+            //"Lucas Noah Tea".Split() -> {"Lucas", "Noah", "Tea"}
+
+            //име -> отпечатваме           
+
+            Action<string> print = name => Console.WriteLine(name);
+            list.ForEach(print);
+        }
+    }
+}
